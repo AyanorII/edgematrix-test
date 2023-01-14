14330c70fc0bb34bbde6a01ef493771be8061512
Ayanori Toyoda <rodrigotoyoda@outlook.com>
2023-01-14 12:48:07 +0900

    add users.json

---
+++ src/interfaces/user.ts
@@ -0,0 +1,54 @@
+export interface User {
+  gender: Gender;
+  name: Name;
+  location: Location;
+  email: string;
+  dob: DateOfBirth;
+  phone: string;
+  cell: string;
+  picture: Picture;
+  nat: string;
+  bio: string;
+  id: string;
+}
+
+export interface DateOfBirth {
+  date: Date;
+  age: number;
+}
+
+export enum Gender {
+  Female = "female",
+  Male = "male",
+}
+
+export interface Location {
+  street: Street;
+  city: string;
+  state: string;
+  country: string;
+  postcode: string | number;
+  timezone: Timezone;
+}
+
+export interface Street {
+  number: number;
+  name: string;
+}
+
+export interface Timezone {
+  offset: string;
+  description: string;
+}
+
+export interface Name {
+  title: string;
+  first: string;
+  last: string;
+}
+
+export interface Picture {
+  large: string;
+  medium: string;
+  thumbnail: string;
+}
