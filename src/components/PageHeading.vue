a4dafeecd5d47b37ccd3c25c1fbde2c6218d4e71
Ayanori Toyoda <rodrigotoyoda@outlook.com>
2023-01-16 10:14:02 +0900

    add PageHeading component

---
+++ src/components/PageHeading.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1 class="heading">
+    <slot></slot>
+  </h1>
+</template>
+
+<style scoped lang="scss">
+.heading {
+  font-size: 2.5rem;
+  font-weight: 500;
+  flex-grow: 1;
+  width: 100%;
+}
+</style>
