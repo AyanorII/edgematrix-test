da84516ff29552f7d6d6905d236f64837d6250eb
Ayanori Toyoda <rodrigotoyoda@outlook.com>
2023-01-16 10:23:57 +0900

    add Card component

---
+++ src/components/Card.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="card">
+    <slot></slot>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.card {
+  border-radius: 4px;
+  display: flex;
+  flex-direction: column;
+  background-color: rgb(68, 67, 67);
+  border-radius: 8px;
+  overflow: hidden;
+  height: 100%;
+}
+</style>
