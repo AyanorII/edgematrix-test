f4a31bef06a7172b6f69f978a1d255a45597ec4c
Ayanori Toyoda <rodrigotoyoda@outlook.com>
2023-01-17 11:40:14 +0900

    add font awesome

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,30 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+  faCakeCandles,
+  faEarthAmericas,
+  faEnvelope,
+  faLocationDot,
+  faMars,
+  faPhone,
+  faVenus,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { createApp } from "vue";
+import App from "./App.vue";
+import "./assets/main.css";
+import router from "./router";
 
-import './assets/main.css'
+const app = createApp(App);
+library.add(
+  faMars,
+  faVenus,
+  faEnvelope,
+  faCakeCandles,
+  faPhone,
+  faLocationDot,
+  faEarthAmericas
+);
 
-const app = createApp(App)
-
-app.use(router)
-
-app.mount('#app')
+app.use(router);
+app.component("font-awesome-icon", FontAwesomeIcon);
+app.mount("#app");
